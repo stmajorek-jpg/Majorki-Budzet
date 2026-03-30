1290b75e6228b36423c15cfbd890d4f5cf094d5a
stmajorek-jpg <stmajorek@gmail.com>
2026-03-30 10:20:20 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,30 +1,18 @@
-const CACHE = 'budzet-v3';
-const BASE = '/Majorki-Budzet/';
+const CACHE = 'budzet-v10';
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll([
-      BASE,
-      BASE + 'index.html',
-      BASE + 'manifest.json',
-      BASE + 'icon-192.png',
-      BASE + 'icon-512.png'
-    ]))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(fetch(e.request));
 });
